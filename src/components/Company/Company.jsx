5330dc059608ba2fb1eb947846b84af3d1893777
shafin bisswas <mdnazmussakibshafin@gmail.com>
2023-05-09 17:46:17 +0600

    created data with json format

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import './Company.css'
 
 const Company = ({ company }) => {
-    const {} = company
+    const { companyName, name, id, companyLogo, companyLocation, salaryRange } = company
     console.log(company)
     return (
-        <div>
-
+        <div className='company-info'>
+            <img src={companyLogo} alt="" />
+            <h2>{name}</h2>
+            <h4>{companyName}</h4>
+            <p>{companyLocation}</p>
         </div>
     );
 };
