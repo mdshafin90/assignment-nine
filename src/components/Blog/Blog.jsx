0826f1b97821ffe13a40d8d10574eb4b5ba1fcc5
shafin bisswas <mdnazmussakibshafin@gmail.com>
2023-05-11 17:15:25 +0600

    assignment marks are showing with pie chart

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -8,19 +8,19 @@ const Blog = () => {
             <div className='blog'>
                 <div className='blog-q-one'>
                     <h1>Q1: When should I use context API ?</h1>
-                    <h3>Ans: When I need to send data to another component to my parent component, I need to use context API because it allows me to send data without having to pass props manually at every component.</h3>
+                    <p>Ans: When I need to send data to another component to my parent component, I need to use context API because it allows me to send data without having to pass props manually at every component.</p>
                 </div>
                 <div className='blog-q-one'>
                     <h1>Q2: What is custom hook ?</h1>
-                    <h3>Ans: A custom hook is a JavaScript function whose name starts with "use" and you can call another hook. If you have a component and you need to extract data from another component this custom hook makes your hook too much easier if you need to change anything in your component.</h3>
+                    <p>Ans: A custom hook is a JavaScript function whose name starts with "use" and you can call another hook. If you have a component and you need to extract data from another component this custom hook makes your hook too much easier if you need to change anything in your component.</p>
                 </div>
                 <div className='blog-q-one'>
                     <h1>Q3: How does useRef works ?</h1>
-                    <h3>Ans: useRef(initialValue) is a built-in React hook that accepts one argument as the initial value and returns a reference. If I have data and that data has a special element then I can use useRef to use that special element.</h3>
+                    <p>Ans: useRef(initialValue) is a built-in React hook that accepts one argument as the initial value and returns a reference. If I have data and that data has a special element then I can use useRef to use that special element.</p>
                 </div>
                 <div className='blog-q-one'>
                     <h1>Q4: How does useMemo works ?</h1>
-                    <h3>Ans: React has a built-in hook called useMemo that allows you to memorize special functions so that you can avoid calling them on every render. You can simply pass an array of input & then useMemo for remember your special functions.</h3>
+                    <p>Ans: React has a built-in hook called useMemo that allows you to memorize special functions so that you can avoid calling them on every render. You can simply pass an array of input & then useMemo for remember your special functions.</p>
                 </div>
             </div>
         </div>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
 import './Statistics.css'
 
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from 'recharts';
+
+const assignmentData = [
+    {
+        name: 'Assignment-One',
+        value: 27,
+        id: 2365
+    },
+    {
+        name: 'Assignment-Two',
+        value: 27,
+        id: 3515
+    },
+    {
+        name: 'Assignment-Three',
+        value: 5,
+        id: 8745
+    },
+    {
+        name: 'Assignment-Four',
+        value: 20,
+        id: 6548
+    },
+    {
+        name: 'Assignment-Five',
+        value: 45,
+        id: "1545"
+    },
+    {
+        name: 'Assignment-Six',
+        value: 5,
+        id: 9454
+    },
+    {
+        name: 'Assignment-Seven',
+        value: 5,
+        id: 3984
+    },
+];
+
 const Statistics = () => {
     return (
         <div>
-            <h1>Statistics</h1>
+            <h1 className='recharts-header'>Assignment Numbers With Recharts</h1>
+            <div className='pie-chart'>
+                <PieChart width={600} height={600}>
+                    <Pie
+                        dataKey="value"
+                        isAnimationActive={false}
+                        data={assignmentData}
+                        cx="50%"
+                        cy="50%"
+                        outerRadius={80}
+                        fill="#8884d8"
+                        label
+                    />
+                    <Legend />
+                    <Tooltip />
+                </PieChart>
+            </div>
         </div>
     );
 };
